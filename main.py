a5f11898e1c5e7a52b5ca9a0ec88d714619e150b
Karyl Dith <karyldith123@gmail.com>
2025-04-15 16:51:08 +0800

    update main.py for multicam features

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from dashboard.dashboard_with_sqlite import *
+# from dashboard.dashboard_with_sqlite import *
+from dashboard.dashboard_for_multicam import *
 
 if __name__ == "__main__":
     app = DashboardApp(theme="darkly")
