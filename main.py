105a7b602e7e91a4bfb84f1e8700bcb8504668c8
Karyl Dith <karyldith123@gmail.com>
2025-05-21 11:42:19 +0800

    changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # from old_codes.dashboard_with_sqlite import *         # For test code
-# from dashboard.dashboard_for_multicam import *          # For Windows machine
-from dashboard.dashboard_raspi import *               # For Raspberry Pi machine
+from dashboard.dashboard_for_multicam import *          # For Windows machine
+# from dashboard.dashboard_raspi import *               # For Raspberry Pi machine
 
 if __name__ == "__main__":
     app = DashboardApp(theme="darkly")
